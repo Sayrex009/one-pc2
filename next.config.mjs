0e3a6119798c6be4c7c57f310914a423ead07012
Sayrex009 <ticho350@gmail.com>
2025-04-19 15:46:07 +0500

    end project 10

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true, // Отключаем оптимизацию изображений
+    domains: ['pc.onepc.uz', 'media.onepc.uz'],
   },
 };
 
